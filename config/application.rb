a56ede9d14706af0437c3148b5c1664d6a4f2e33
Damian Śliwecki <sliwecki@gmail.com>
2015-03-17 00:36:06 +0100

    add error sites

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 require 'roo'
-require 'spreadsheet'
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+
 Bundler.require(*Rails.groups)
 
 A9n.root = File.expand_path('../..', __FILE__)
