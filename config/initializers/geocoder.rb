452e076d142503c47d913253a762ca5d42e9fb11
Damian Śliwecki <sliwecki@gmail.com>
2015-06-23 14:40:25 +0200

    fix registration

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,20 +1,18 @@
 Geocoder.configure(
-  :timeout      => 15,           # geocoding service timeout (secs)
-  #:lookup       => :bing,     # name of geocoding service (symbol)
-  # :language     => :en,         # ISO-639 language code
+  timeout:      15,               # geocoding service timeout (secs)
+  #:lookup       => :bing,        # name of geocoding service (symbol)
+  language:     :pl,              # ISO-639 language code
   # :use_https    => false,       # use HTTPS for lookup requests? (if supported)
   # :http_proxy   => nil,         # HTTP proxy server (user:pass@host:port)
   # :https_proxy  => nil,         # HTTPS proxy server (user:pass@host:port)
-  #:api_key      => A9n.bing_api_key,         # API key for geocoding service
-  :cache        => Rails.cache,         # cache object (must respond to #[], #[]=, and #keys)
-  :cache_prefix => "geocoder:", # prefix (string) to use for all cache keys
-
+  #:api_key       => A9n.bing_api_key,         # API key for geocoding service
+  cache:        Rails.cache,      # cache object (must respond to #[], #[]=, and #keys)
+  cache_prefix: "geocoder:",      # prefix (string) to use for all cache keys
   # exceptions that should not be rescued by default
   # (if you want to implement custom error handling);
   # supports SocketError and TimeoutError
   # :always_raise => [],
-
   # calculation options
-  :units     => :km,       # :km for kilometers or :mi for miles
+  units:        :km,         # :km for kilometers or :mi for miles
   # :distances => :linear    # :spherical or :linear
 )
--- config/initializers/redis.rb
+++
@@ -1,1 +0,0 @@
-REDIS = Redis.new(url: ENV['REDISTOGO_URL'])
