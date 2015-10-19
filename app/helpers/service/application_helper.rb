da865ef52ae082b7cbb29b9a0c77721959327f93
Jakub Lasek <jakub@futurecode.pl>
2015-10-19 13:18:47 +0200

    zut - default avatar

--- app/helpers/service/application_helper.rb
+++ app/helpers/service/application_helper.rb
@@ -1,9 +1,8 @@
 module Service
   module ApplicationHelper
 
-    def avatar_path(user = nil)
-      user ||= current_admin
-      user.avatar? ? user.avatar_url : 'avatar.png'
+    def avatar_path
+      current_admin.avatar? ? current_admin.avatar_url : 'avatar.png'
     end
   end
 end
