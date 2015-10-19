9ea37059c59de53dac7f92458d244904e5ae7e8a
Jakub Lasek <jakub@futurecode.pl>
2015-10-19 13:12:48 +0200

    Avatar

--- app/helpers/service/application_helper.rb
+++ app/helpers/service/application_helper.rb
@@ -1,8 +1,9 @@
 module Service
   module ApplicationHelper
 
-    def avatar_path
-      current_admin.avatar? ? current_admin.avatar_url : 'avatar.png'
+    def avatar_path(user = nil)
+      user ||= current_admin
+      user.avatar? ? user.avatar_url : 'avatar.png'
     end
   end
 end
