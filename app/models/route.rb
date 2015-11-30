7cb1eb4eb09c674e188a94fc049b911f957c814b
ekubal <kuba@ekubal.pl>
2015-11-30 11:39:25 +0100

    Routes - admin

--- app/models/route.rb
+++ app/models/route.rb
@@ -3,15 +3,6 @@ class Route < ActiveRecord::Base
   has_many :route_versions, dependent: :destroy
   
   validates :name, presence: true, uniqueness: true
-  validates :area_id, presence: true, inclusion: {in: (1..22)}
-
-  def self.area_list
-    (1..22).map {|inx| ["Sektor #{inx}", inx] }
-  end
-
-  def area
-    "Sektor #{self.area_id}" if self.area_id
-  end
 
   def to_s
     self.name
